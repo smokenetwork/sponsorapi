51a97a9daa1980a7a7ff38670b542e4301902b57
Tuan Pham Anh <baabeetaa@gmail.com>
2019-03-22 17:27:14 +0700

    update about and website

--- api.js
+++ api.js
@@ -2,12 +2,16 @@ let express = require('express');
 const db = require('./db');
 const utils = require('./utils');
 const NodeCache = require( "node-cache" );
+const smokejs = require('@smokenetwork/smoke-js');
 
 const myCache = new NodeCache( { stdTTL: 60, checkperiod: 60 } );
 
 let app = express();
-
 const port = process.env.PORT || 8200;
+
+smokejs.api.setOptions({url: 'https://pubrpc.smoke.io'});
+smokejs.config.set('address_prefix', 'SMK');
+smokejs.config.set('chain_id', '1ce08345e61cd3bf91673a47fc507e7ed01550dab841fd9cdb0ab66ef576aaf0');
 
 fetch_data = async () => {
   let data = myCache.get("data");
@@ -16,6 +20,42 @@ fetch_data = async () => {
     // do nothing
   } else {
     data = await db.getDB().all('SELECT * FROM sponsor ORDER BY amount DESC');
+
+    {
+      // update about and website
+      let usernames = data.map(item => item.accountname);
+      const accounts = await smokejs.api.getAccountsAsync(usernames);
+
+      let metadatas = {};
+      accounts.forEach((item) => {
+        try {
+          const name = item['name'];
+
+          let metadata = item['json_metadata'];
+          if (typeof metadata === 'string') {
+            metadata = JSON.stringify(metadata);
+          }
+          let about = "Smoke Sponsor"; // default
+          let website = "https://smoke.io"; // default
+
+          try { about = metadata['profile']['about']; } catch (err) {}
+          try { website = metadata['profile']['website']; } catch (err) {}
+
+          metadatas[name] = {
+            about,
+            website
+          }
+
+        } catch (e) {}
+      });
+
+      console.log(JSON.stringify(metadatas));
+
+      // update to data
+      data = data.map( item => {
+        return Object.assign(item, metadatas[item.accountname]);
+      });
+    }
 
     // remember to set cache
     myCache.set("data", data);
